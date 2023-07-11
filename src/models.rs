73c6c30c0b6e25353d912c9ffeac376ab4f6ac24
yummi <Leeo05050@protonmail.com>
2023-07-11 18:42:14 -0300

    gpt4 32k

--- src/models.rs
+++ src/models.rs
@@ -7,6 +7,7 @@ use sqlx::Type;
 pub enum Models {
     Gpt3,
     Gpt4,
+    Gpt4_32K
 }
 
 impl Models {
@@ -14,6 +15,7 @@ impl Models {
         let model = match self {
             Models::Gpt3 => "gpt-3.5-turbo",
             Models::Gpt4 => "gpt-4",
+            Models::Gpt4_32K => "gpt-4-32k",
         };
         model.to_string()
     }
